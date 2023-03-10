2e576d5d4179afb12f8507bfd2d484ebbf20114a
Juwan Gross <juwangross0815@gmail.com>
2023-03-10 18:13:38 -0500

    commit 4

--- quizzey/src/index.js
+++ quizzey/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('ORIGIN: ', window.location.origin)
 root.render(
   <Auth0Provider
     domain='dev-ha6q1jxc.us.auth0.com'
