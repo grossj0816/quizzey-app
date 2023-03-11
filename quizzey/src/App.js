d577fc636ec8274ecd70a8cdcf0ea1934d9f765f
Juwan Gross <juwangross0815@gmail.com>
2023-03-11 17:43:11 -0500

    added /html in path

--- quizzey/src/App.js
+++ quizzey/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={!isAuthenticated ? <LandingPage /> : <Dashboard/>} />
+        <Route path='/index.html' element={!isAuthenticated ? <LandingPage /> : <Dashboard/>} />
       </Routes>
     </BrowserRouter>
   );
