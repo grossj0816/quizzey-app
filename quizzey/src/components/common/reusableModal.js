a4f4fbd2790fddcb70883504231cd8b2a7a6c685
Juwan Gross <juwangross0815@gmail.com>
2023-10-16 12:09:25 -0400

    Removed a from reusableModel.js

--- quizzey/src/components/common/reusableModal.js
+++ quizzey/src/components/common/reusableModal.js
@@ -41,7 +41,6 @@ const ReusableModal = (props) => {
             <Modal.Body 
             // style={{fontSize: adjustTextSize(innerWidth)}} 
             >
-                a
                 {body()}
             </Modal.Body>
             <Modal.Footer>
